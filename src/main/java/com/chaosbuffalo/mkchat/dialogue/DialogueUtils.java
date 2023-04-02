9a6b604ced78f8048c9f33f1384947e4a27d0aec
Lee Harrison <ralekdev@gmail.com>
2023-04-02 14:53:00 -0700

    Remove calls to getRegistryName

--- src/main/java/com/chaosbuffalo/mkchat/dialogue/DialogueUtils.java
+++ src/main/java/com/chaosbuffalo/mkchat/dialogue/DialogueUtils.java
@@ -1,6 +1,7 @@
 package com.chaosbuffalo.mkchat.dialogue;
 
 import com.chaosbuffalo.mkchat.MKChat;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -9,6 +10,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.Util;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class DialogueUtils {
     private static final double CHAT_RADIUS = 5.0;
@@ -23,11 +25,13 @@ public class DialogueUtils {
     }
 
     public static String getItemNameProvider(Item item) {
-        return String.format("{item:%s}", item.getRegistryName().toString());
+        ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
+        return String.format("{item:%s}", id.toString());
     }
 
     public static String getStackCountItemProvider(ItemStack item) {
-        return String.format("%d {item:%s}", item.getCount(), item.getItem().getRegistryName().toString());
+        ResourceLocation id = ForgeRegistries.ITEMS.getKey(item.getItem());
+        return String.format("%d {item:%s}", item.getCount(), id.toString());
     }
 
     public static void throwParseException(String message) {
